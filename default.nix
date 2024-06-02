c370d1ba647038f88246553895805515eeddf0ce
GGetsov <g.getsov.dev@gmail.com>
2024-06-02 03:24:04 +0300

    Attribute set

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
-(final: prev: {
-  gnome = prev.gnome.overrideScope (finalg: prevg: {
-    gnome-shell = prevg.gnome-shell.overrideAttrs (attrs: {
-      postInstall = (attrs.postInstall or "") + ''
-      glib-compile-resources ${./gnome-shell/gnome-shell-theme.gresource.xml} --sourcedir=${./gnome-shell} --target=$out/share/gnome-shell/gnome-shell-theme.gresource
-      '';
+{
+  gnome-shell-theme = (final: prev: {
+    gnome = prev.gnome.overrideScope (finalg: prevg: {
+      gnome-shell = prevg.gnome-shell.overrideAttrs (attrs: {
+        postInstall = (attrs.postInstall or "") + ''
+        glib-compile-resources ${./gnome-shell/gnome-shell-theme.gresource.xml} --sourcedir=${./gnome-shell} --target=$out/share/gnome-shell/gnome-shell-theme.gresource
+        '';
+      });
     });
   });
-})
+}
