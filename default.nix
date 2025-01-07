1519fbe15530a7a505290b90ae5283427403cf61
GGetsov <g.getsov.dev@gmail.com>
2025-01-07 23:57:17 +0200

    Test update

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 {
   gnome-shell-theme = (final: prev: {
-    gnome = prev.gnome.overrideScope (finalg: prevg: {
-      gnome-shell = prevg.gnome-shell.overrideAttrs (attrs: {
+    # gnome = prev.gnome.overrideScope (finalg: prevg: {
+      # gnome-shell = prevg.gnome-shell.overrideAttrs (attrs: {
+      gnome-shell = prev.gnome-shell.overrideAttrs (attrs: {
         postInstall = (attrs.postInstall or "") + ''
         glib-compile-resources ${./gnome-shell/gnome-shell-theme.gresource.xml} --sourcedir=${./gnome-shell} --target=$out/share/gnome-shell/gnome-shell-theme.gresource
         '';
